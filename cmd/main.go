33749d569958206f01e9bbd24ea9820a7167db92
Kowshik Ram R <kowshik.r@J7Q7L754M2-Pleo-Mac.local>
2025-03-05 01:50:59 +0530

    Added routes to main function and also a home page

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,44 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"money-transfer/internal/handler"
+	"money-transfer/internal/repository"
+	"money-transfer/internal/service"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	// Simple welcome message
+	fmt.Fprintf(w, "Welcome to Money Transfer Service")
+}
+
+func methodHandler(allowedMethod string, handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != allowedMethod {
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		handler(w, r)
+	}
+}
 
 func main() {
-	// Start the server
-	log.Println("Starting server")
+	// Create dependencies
+	accountRepo := repository.NewAccountRepository()
+	transferService := service.NewTransferService(accountRepo)
+
+	// Create handlers
+	accountHandler := handler.NewAccountHandler(accountRepo)
+	transferHandler := handler.NewTransferHandler(transferService)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", methodHandler(http.MethodGet, homeHandler))
+	mux.HandleFunc("/account/create", methodHandler(http.MethodPost, accountHandler.CreateAccount))
+	mux.HandleFunc("/account/balance", methodHandler(http.MethodGet, accountHandler.GetAccountBalance))
+	mux.HandleFunc("/transfer", methodHandler(http.MethodPost, transferHandler.Transfer))
+
+	log.Println("Server started at :8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
